16d76817dffca6d0462571988002dcbcc3a88a01
Matthew Smith <m@lattejed.com>
2022-03-16 15:49:59 +0700

    Format and lint

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.11;
+pragma solidity ^0.8.10;
 
-import {DSTest} from "@ds/test.sol";
-import {ERC20} from "@solmate/tokens/ERC20.sol";
-
-import {stdCheats, stdError} from "@std/stdlib.sol";
-import {Vm} from "@std/Vm.sol";
+import { DSTest } from "@ds/test.sol";
+import { ERC20 } from "@solmate/tokens/ERC20.sol";
+import { stdCheats, stdError } from "@std/stdlib.sol";
+import { Vm } from "@std/Vm.sol";
 
 contract DSTestPlus is DSTest, stdCheats {
+  /// @dev Use forge-std Vm logic
+  // solhint-disable-next-line const-name-snakecase
+  Vm public constant vm = Vm(HEVM_ADDRESS);
 
-    /// @dev Use forge-std Vm logic
-    Vm public constant vm = Vm(HEVM_ADDRESS);
-
-    function assertERC20Eq(ERC20 erc1, ERC20 erc2) internal {
-        assertEq(address(erc1), address(erc2));
-    }
+  function _assertERC20Eq(ERC20 erc1, ERC20 erc2) internal {
+    assertEq(address(erc1), address(erc2));
+  }
 }
