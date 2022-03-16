11289900f41076df89eea8ca6e85736b5753d03a
Matthew Smith <m@lattejed.com>
2022-03-17 03:27:54 +0700

    Updated remappings to use forge output

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.10;
 
-import { DSTest } from "@ds/test.sol";
-import { ERC20 } from "@solmate/tokens/ERC20.sol";
-import { stdCheats, stdError } from "@std/stdlib.sol";
-import { Vm } from "@std/Vm.sol";
+import { DSTest } from "ds-test/test.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { stdCheats, stdError } from "forge-std/stdlib.sol";
+import { Vm } from "forge-std/Vm.sol";
 
 contract DSTestPlus is DSTest, stdCheats {
   /// @dev Use forge-std Vm logic
