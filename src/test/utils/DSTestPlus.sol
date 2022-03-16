08d97af80acf968068456b3288480e0d288b9b47
Matthew Smith <m@lattejed.com>
2022-03-17 03:13:20 +0700

    Removed solhint

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -8,7 +8,6 @@ import { Vm } from "@std/Vm.sol";
 
 contract DSTestPlus is DSTest, stdCheats {
   /// @dev Use forge-std Vm logic
-  // solhint-disable-next-line const-name-snakecase
   Vm public constant vm = Vm(HEVM_ADDRESS);
 
   function _assertERC20Eq(ERC20 erc1, ERC20 erc2) internal {
